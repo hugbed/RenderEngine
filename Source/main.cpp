d006f9aa71a2556b5c7b9b29dc85b51ff70ce9c8
Hugo Bedard <hugo_bedard@hotmail.com>
2019-11-03 20:22:04 -0500

    Add synchronization and resize

--- Source/main.cpp
+++ Source/main.cpp
@@ -12,107 +12,291 @@
 #include "Swapchain.h"
 #include "RenderPass.h"
 
-int main()
+class CommandBuffers
 {
-	vk::Extent2D extent{ 800, 600 };
-
-	Window window(extent, "Vulkan");
-
-	Instance instance(window);
-
-	vk::UniqueSurfaceKHR surface = window.CreateSurface(instance.Get());
-
-	PhysicalDevice physicalDevice(instance.Get(), surface.get());
-
-	Device device(physicalDevice);
-
-	// Queues
-	auto indices = physicalDevice.GetQueueFamilies();
-	auto graphicsQueue = device.GetQueue(indices.graphicsFamily.value());
-	auto presentQueue = device.GetQueue(indices.presentFamily.value());
-
-	Swapchain swapchain(
-		device,
-		physicalDevice,
-		surface.get(),
-		extent
-	);
-
-	RenderPass renderPass(device.Get(), swapchain);
-
-	// Commands
-
-	// Pool
-	vk::CommandPoolCreateInfo poolInfo({}, indices.graphicsFamily.value());
-	vk::UniqueCommandPool commandPool = device.Get().createCommandPoolUnique(poolInfo);
-
-	// Buffer
-	vk::CommandBufferAllocateInfo commandBufferAllocateInfo(
-		commandPool.get(), vk::CommandBufferLevel::ePrimary, swapchain.GetImageCount()
-	);
-
-	// Create command buffers
-	std::vector<vk::UniqueCommandBuffer> commandBuffers = device.Get().allocateCommandBuffersUnique(commandBufferAllocateInfo);
-
-	// Record commands
-	for (size_t i = 0; i < commandBuffers.size(); i++)
+public:
+	CommandBuffers(vk::Device device, size_t count, uint32_t queueFamily)
 	{
-		auto& commandBuffer = commandBuffers[i];
-		commandBuffer->begin(vk::CommandBufferBeginInfo());
-		renderPass.SendRenderCommands(commandBuffer.get(), i);
-		commandBuffer->end();
+		// Pool
+		vk::CommandPoolCreateInfo poolInfo({}, queueFamily);
+		m_commandPool = device.createCommandPoolUnique(poolInfo);
+		
+		Reset(device, count);
 	}
 
-	// Synchronization
+	vk::CommandBuffer Get(uint32_t imageIndex)
+	{
+		return m_commandBuffers[imageIndex].get();
+	}
 
-	vk::UniqueSemaphore imageAvailableSemaphore = device.Get().createSemaphoreUnique({});
-	vk::UniqueSemaphore renderFinishedSemaphore = device.Get().createSemaphoreUnique({});
+	void Reset(vk::Device device, size_t count)
+	{
+		// Buffer
+		vk::CommandBufferAllocateInfo commandBufferAllocateInfo(
+			m_commandPool.get(), vk::CommandBufferLevel::ePrimary, static_cast<uint32_t>(count)
+		);
 
-	window.MainLoop(
-		[&device, &swapchain, &imageAvailableSemaphore, &renderFinishedSemaphore, &commandBuffers, &graphicsQueue, &presentQueue]()
+		// Command buffers
+		m_commandBuffers = device.allocateCommandBuffersUnique(commandBufferAllocateInfo);
+	}
+
+private:
+	vk::UniqueCommandPool m_commandPool;
+	std::vector<vk::UniqueCommandBuffer> m_commandBuffers;
+};
+
+class SynchronizationPrimitives
+{
+public:
+	SynchronizationPrimitives(vk::Device device, uint32_t swapchainImagesCount, size_t maxFramesInFlight)
+		: m_maxFramesInFlight(maxFramesInFlight)
+		, device(device)
+	{
+		m_imageAvailableSemaphores.reserve(maxFramesInFlight);
+		m_renderFinishedSemaphores.reserve(maxFramesInFlight);
+		m_inFlightFences.reserve(maxFramesInFlight);
+		m_imagesInFlight.resize(swapchainImagesCount);
+
+		for (size_t i = 0; i < maxFramesInFlight; ++i)
 		{
-			// Acquire image from swapchain (this could be in swapchain?)
-			auto [result, imageIndex] = device.Get().acquireNextImageKHR(swapchain.Get(), uint64_t(UINT64_MAX), imageAvailableSemaphore.get(), nullptr);
-			if (result != vk::Result::eSuccess)
-				return;
-
-			// Submit command buffer on graphics queue
-			vk::Semaphore waitSemaphores[] = { imageAvailableSemaphore.get() };
-			vk::PipelineStageFlags waitStages[] = { vk::PipelineStageFlagBits::eColorAttachmentOutput };
-			vk::Semaphore signalSemaphores[] = { renderFinishedSemaphore.get() };
-			vk::SubmitInfo submitInfo(
-				1, waitSemaphores, waitStages,
-				1, &commandBuffers[imageIndex].get(),
-				1, signalSemaphores
-			);
-			graphicsQueue.submit(submitInfo, nullptr);
-
-			// Subpass dependencies
-			vk::SubpassDependency dependency;
-			dependency.srcSubpass = VK_SUBPASS_EXTERNAL;
-			dependency.dstSubpass = 0;
-			dependency.srcStageMask = vk::PipelineStageFlagBits::eColorAttachmentOutput;
-			dependency.srcAccessMask = vk::AccessFlags();
-			dependency.dstStageMask = vk::PipelineStageFlagBits::eColorAttachmentOutput;
-			dependency.dstAccessMask = vk::AccessFlagBits::eColorAttachmentRead | vk::AccessFlagBits::eColorAttachmentWrite;
-
-			// Presentation
-			vk::PresentInfoKHR presentInfo = {};
-			presentInfo.waitSemaphoreCount = 1;
-			presentInfo.pWaitSemaphores = signalSemaphores;
-			
-			vk::SwapchainKHR swapChains[] = { swapchain.Get() };
-			presentInfo.swapchainCount = 1;
-			presentInfo.pSwapchains = swapChains;
-			presentInfo.pImageIndices = &imageIndex;
-
-			result = presentQueue.presentKHR(presentInfo);
-			if (result == vk::Result::eSuboptimalKHR || result == vk::Result::eErrorOutOfDateKHR)
-				return; // recreate swapchain
-
-			vkQueueWaitIdle(presentQueue);
+			m_imageAvailableSemaphores.push_back(device.createSemaphoreUnique({}));
+			m_renderFinishedSemaphores.push_back(device.createSemaphoreUnique({}));
+			m_inFlightFences.push_back(device.createFenceUnique(
+				vk::FenceCreateInfo(vk::FenceCreateFlagBits::eSignaled)
+			));
 		}
-	);
+	}
 
+	const vk::Semaphore& GetImageAvailableSemaphore() const
+	{
+		return m_imageAvailableSemaphores[m_currentFrame].get();
+	}
+
+	const vk::Semaphore& GetRenderFinishedSemaphore() const
+	{
+		return m_renderFinishedSemaphores[m_currentFrame].get();
+	}
+
+	vk::Fence& WaitForFrame()
+	{
+		auto& frameFence = m_inFlightFences[m_currentFrame].get();
+		device.waitForFences(
+			frameFence,
+			true, // wait for all fences (we only have 1 though)
+			UINT64_MAX // indefinitely
+		);
+		return frameFence;
+	}
+
+	void WaitUntilImageIsAvailable(uint32_t imageIndex)
+	{
+		// Check if a previous frame is using this image (i.e. there is its fence to wait on)
+		if (m_imagesInFlight[imageIndex])
+		{
+			device.waitForFences(m_imagesInFlight[imageIndex], true, UINT64_MAX);
+		}
+
+		// Mark the image as now being in use by this frame
+		m_imagesInFlight[imageIndex] = m_inFlightFences[m_currentFrame].get();
+	}
+
+	const vk::Fence& GetFrameFence()
+	{
+		return m_inFlightFences[m_currentFrame].get();
+	}
+
+	void MoveToNextFrame()
+	{
+		m_currentFrame = (m_currentFrame + 1) % m_maxFramesInFlight;
+	}
+
+private:
+	vk::Device device;
+
+	size_t m_currentFrame = 0;
+	int m_maxFramesInFlight = 2;
+	std::vector<vk::UniqueSemaphore> m_imageAvailableSemaphores;
+	std::vector<vk::UniqueSemaphore> m_renderFinishedSemaphores;
+	std::vector<vk::UniqueFence> m_inFlightFences;
+	std::vector<vk::Fence> m_imagesInFlight;
+};
+
+class App
+{
+public:
+	static constexpr size_t kMaxFramesInFlight = 2;
+
+	App()
+		: extent(800, 600)
+		, window(extent, "Vulkan")
+		, instance(window)
+		, surface(window.CreateSurface(instance.Get()))
+		, physicalDevice(instance.Get(), surface.get())
+		, device(physicalDevice)
+		, swapchain(std::make_unique<Swapchain>(device, physicalDevice, surface.get(), extent))
+		, renderPass(std::make_unique<RenderPass>(device.Get(), *swapchain))
+		, commandBuffers(
+			device.Get(),
+			(*renderPass).GetFrameBufferCount(),
+			physicalDevice.GetQueueFamilies().graphicsFamily.value()
+		 )
+		, syncPrimitives(device.Get(), swapchain->GetImageCount(), kMaxFramesInFlight)
+	{
+		auto indices = physicalDevice.GetQueueFamilies();
+		graphicsQueue = device.GetQueue(indices.graphicsFamily.value());
+		presentQueue = device.GetQueue(indices.presentFamily.value());
+		
+		window.SetWindowResizeCallback(reinterpret_cast<void*>(this), OnResize);
+
+		RecordRenderPass();
+	}
+
+	void Run()
+	{
+		window.MainLoop([this]() { Render(); });
+
+		vkDeviceWaitIdle(device.Get());
+	}
+
+	static void OnResize(void* data, int w, int h)
+	{
+		auto app = reinterpret_cast<App*>(data);
+		app->frameBufferResized = true;
+	}
+
+	void RecordRenderPass()
+	{
+		// Record commands
+		for (size_t i = 0; i < swapchain->GetImageCount(); i++)
+		{
+			auto& commandBuffer = commandBuffers.Get(i);
+			commandBuffer.begin(vk::CommandBufferBeginInfo());
+			renderPass->SendRenderCommands(commandBuffer, i);
+			commandBuffer.end();
+		}
+	}
+
+	void Render()
+	{
+		auto frameFence = syncPrimitives.WaitForFrame();
+
+		auto [result, imageIndex] = device.Get().acquireNextImageKHR(
+			swapchain->Get(),
+			UINT64_MAX, // max timeout
+			syncPrimitives.GetImageAvailableSemaphore(),
+			nullptr
+		);
+		if (result == vk::Result::eErrorOutOfDateKHR) // todo: this one will throw
+		{
+			RecreateSwapchain();
+			return;
+		}
+
+		syncPrimitives.WaitUntilImageIsAvailable(imageIndex);
+
+		// Submit command buffer on graphics queue
+		vk::Semaphore imageAvailableSemaphores[] = { syncPrimitives.GetImageAvailableSemaphore() };
+		vk::PipelineStageFlags waitStages[] = { vk::PipelineStageFlagBits::eColorAttachmentOutput };
+		vk::Semaphore renderFinishedSemaphores[] = { syncPrimitives.GetRenderFinishedSemaphore() };
+		vk::SubmitInfo submitInfo(
+			1, imageAvailableSemaphores,
+			waitStages,
+			1, &commandBuffers.Get(imageIndex),
+			1, renderFinishedSemaphores
+		);
+		device.Get().resetFences(frameFence); // reset right before submit
+		graphicsQueue.submit(submitInfo, frameFence);
+
+		// Subpass dependencies
+		vk::SubpassDependency dependency;
+		dependency.srcSubpass = VK_SUBPASS_EXTERNAL;
+		dependency.dstSubpass = 0;
+		dependency.srcStageMask = vk::PipelineStageFlagBits::eColorAttachmentOutput;
+		dependency.srcAccessMask = vk::AccessFlags();
+		dependency.dstStageMask = vk::PipelineStageFlagBits::eColorAttachmentOutput;
+		dependency.dstAccessMask = vk::AccessFlagBits::eColorAttachmentRead | vk::AccessFlagBits::eColorAttachmentWrite;
+
+		// Presentation
+		vk::PresentInfoKHR presentInfo = {};
+		presentInfo.waitSemaphoreCount = 1;
+		presentInfo.pWaitSemaphores = renderFinishedSemaphores;
+
+		vk::SwapchainKHR swapChains[] = { swapchain->Get() };
+		presentInfo.swapchainCount = 1;
+		presentInfo.pSwapchains = swapChains;
+		presentInfo.pImageIndices = &imageIndex;
+
+		result = presentQueue.presentKHR(presentInfo);
+		if (result == vk::Result::eSuboptimalKHR || 
+			result == vk::Result::eErrorOutOfDateKHR || // todo: this one will throw
+			frameBufferResized)
+		{
+			frameBufferResized = false;
+			RecreateSwapchain();
+			return;
+		}
+		else if (result != vk::Result::eSuccess)
+		{
+			throw std::runtime_error("Failed to acquire swapchain image");
+		}
+
+		syncPrimitives.MoveToNextFrame();
+	}
+
+	void RecreateSwapchain()
+	{
+		extent = window.GetFramebufferSize();
+	
+		// Wait if window is minimized
+		while (extent.width == 0 || extent.height == 0)
+		{
+			extent = window.GetFramebufferSize();
+			window.WaitForEvents();
+		}
+
+		device.Get().waitIdle();
+
+		// Recreate swapchain and render pass
+		renderPass.reset();
+		swapchain.reset();
+
+		swapchain = std::make_unique<Swapchain>(
+			device,
+			physicalDevice,
+			surface.get(),
+			extent
+		);
+		extent = swapchain->GetImageExtent();
+
+		renderPass = std::make_unique<RenderPass>(device.Get(), *swapchain);
+
+		commandBuffers.Reset(device.Get(), swapchain->GetImageCount());
+		RecordRenderPass();
+	}
+
+private:
+	vk::Extent2D extent;
+	Window window;
+	bool frameBufferResized{ false };
+	Instance instance;
+	vk::UniqueSurfaceKHR surface;
+	PhysicalDevice physicalDevice;
+	Device device;
+	
+	std::unique_ptr<Swapchain> swapchain;
+	std::unique_ptr<RenderPass> renderPass;
+	CommandBuffers commandBuffers;
+
+	vk::Queue graphicsQueue;
+	vk::Queue presentQueue;
+
+	// Synchronization
+	SynchronizationPrimitives syncPrimitives;
+};
+
+int main()
+{
+	App app;
+	app.Run();
 	return 0;
 }
