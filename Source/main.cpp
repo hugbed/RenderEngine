2a84375e68bf81dcaf704b3dc51172b7d7ee73a6
Hugo Bedard <hugo_bedard@hotmail.com>
2019-11-05 20:38:46 -0500

    Move commands and sync to separate files

--- Source/main.cpp
+++ Source/main.cpp
@@ -11,115 +11,8 @@
 #include "PhysicalDevice.h"
 #include "Swapchain.h"
 #include "RenderPass.h"
-
-class CommandBuffers
-{
-public:
-	CommandBuffers(vk::Device device, size_t count, uint32_t queueFamily)
-	{
-		// Pool
-		vk::CommandPoolCreateInfo poolInfo({}, queueFamily);
-		m_commandPool = device.createCommandPoolUnique(poolInfo);
-		
-		Reset(device, count);
-	}
-
-	vk::CommandBuffer Get(uint32_t imageIndex)
-	{
-		return m_commandBuffers[imageIndex].get();
-	}
-
-	void Reset(vk::Device device, size_t count)
-	{
-		// Buffer
-		vk::CommandBufferAllocateInfo commandBufferAllocateInfo(
-			m_commandPool.get(), vk::CommandBufferLevel::ePrimary, static_cast<uint32_t>(count)
-		);
-
-		// Command buffers
-		m_commandBuffers = device.allocateCommandBuffersUnique(commandBufferAllocateInfo);
-	}
-
-private:
-	vk::UniqueCommandPool m_commandPool;
-	std::vector<vk::UniqueCommandBuffer> m_commandBuffers;
-};
-
-class SynchronizationPrimitives
-{
-public:
-	SynchronizationPrimitives(vk::Device device, uint32_t swapchainImagesCount, size_t maxFramesInFlight)
-		: m_maxFramesInFlight(maxFramesInFlight)
-		, device(device)
-	{
-		m_imageAvailableSemaphores.reserve(maxFramesInFlight);
-		m_renderFinishedSemaphores.reserve(maxFramesInFlight);
-		m_inFlightFences.reserve(maxFramesInFlight);
-		m_imagesInFlight.resize(swapchainImagesCount);
-
-		for (size_t i = 0; i < maxFramesInFlight; ++i)
-		{
-			m_imageAvailableSemaphores.push_back(device.createSemaphoreUnique({}));
-			m_renderFinishedSemaphores.push_back(device.createSemaphoreUnique({}));
-			m_inFlightFences.push_back(device.createFenceUnique(
-				vk::FenceCreateInfo(vk::FenceCreateFlagBits::eSignaled)
-			));
-		}
-	}
-
-	const vk::Semaphore& GetImageAvailableSemaphore() const
-	{
-		return m_imageAvailableSemaphores[m_currentFrame].get();
-	}
-
-	const vk::Semaphore& GetRenderFinishedSemaphore() const
-	{
-		return m_renderFinishedSemaphores[m_currentFrame].get();
-	}
-
-	vk::Fence& WaitForFrame()
-	{
-		auto& frameFence = m_inFlightFences[m_currentFrame].get();
-		device.waitForFences(
-			frameFence,
-			true, // wait for all fences (we only have 1 though)
-			UINT64_MAX // indefinitely
-		);
-		return frameFence;
-	}
-
-	void WaitUntilImageIsAvailable(uint32_t imageIndex)
-	{
-		// Check if a previous frame is using this image (i.e. there is its fence to wait on)
-		if (m_imagesInFlight[imageIndex])
-		{
-			device.waitForFences(m_imagesInFlight[imageIndex], true, UINT64_MAX);
-		}
-
-		// Mark the image as now being in use by this frame
-		m_imagesInFlight[imageIndex] = m_inFlightFences[m_currentFrame].get();
-	}
-
-	const vk::Fence& GetFrameFence()
-	{
-		return m_inFlightFences[m_currentFrame].get();
-	}
-
-	void MoveToNextFrame()
-	{
-		m_currentFrame = (m_currentFrame + 1) % m_maxFramesInFlight;
-	}
-
-private:
-	vk::Device device;
-
-	size_t m_currentFrame = 0;
-	int m_maxFramesInFlight = 2;
-	std::vector<vk::UniqueSemaphore> m_imageAvailableSemaphores;
-	std::vector<vk::UniqueSemaphore> m_renderFinishedSemaphores;
-	std::vector<vk::UniqueFence> m_inFlightFences;
-	std::vector<vk::Fence> m_imagesInFlight;
-};
+#include "CommandBuffers.h"
+#include "SynchronizationPrimitives.h"
 
 class App
 {
@@ -137,7 +30,7 @@ public:
 		, renderPass(std::make_unique<RenderPass>(device.Get(), *swapchain))
 		, commandBuffers(
 			device.Get(),
-			(*renderPass).GetFrameBufferCount(),
+			renderPass->GetFrameBufferCount(),
 			physicalDevice.GetQueueFamilies().graphicsFamily.value()
 		 )
 		, syncPrimitives(device.Get(), swapchain->GetImageCount(), kMaxFramesInFlight)
