0e0e2124d58ab1aa273edddfc410e7fca9971d17
hbedard <hbedard@audiokinetic.com>
2019-12-09 19:36:15 -0500

    Use assimp for scene loading + support push constants + support multiple material instances

--- Source/Samples/MainSample/Scripts/compile-shaders.bat
+++
@@ -1,2 +0,0 @@
-glslc.exe %~dp0..\Shaders\mvp.vert -o %~dp0..\..\..\..\Build\Source\Samples\MainSample\Debug\mvp_vert.spv
-glslc.exe %~dp0..\Shaders\surface.frag -o %~dp0..\..\..\..\Build\Source\Samples\MainSample\Debug\surface_frag.spv
